6d6d4d45f734e4c7bde8ee6aee98759e1483ff4f
dolphin2410 <dolphin2410@outlook.kr>
2021-11-20 17:08:06 +0900

    Remove javafx.swing

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module McSR.main {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
-    requires javafx.swing;
     requires javafx.base;
     requires java.net.http;
     requires kotlinx.coroutines.core.jvm;
