99774ae5bdccddae780747c3e03c5c5b8660f58a
dolphin2410 <dolphin2410@outlook.kr>
2021-12-04 22:56:34 +0900

    Fix Binary Error

--- mcsr/src/main/java/module-info.java
+++ mcsr/src/main/java/module-info.java
@@ -1,10 +1,9 @@
 open module mcsr {
-    requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.graphics;
-    requires javafx.base;
     requires javafx.web;
     requires java.desktop;
+    requires java.net.http;
+    requires jdk.crypto.ec;
     requires api;
 
     exports io.github.dolphin2410.mcsr;
